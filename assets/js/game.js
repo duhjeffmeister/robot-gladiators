3154ffa16f64d2746b477eddd329673bbe0b4667
Jeff Small <jeffmeister1990@gmail.com>
2022-05-10 18:24:46 -0400

    Created some variables and set up Console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an alert! JavaScript is running!");
+// This creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// Robot Name
+var playerName = window.prompt("What is your robot's name?");
+
+// What is this?
+console.log(playerName);
+// This will do math and log20
+console.log(10+10);
+//What is this?
+console.log("Our robot's name is " + playerName);
+
+var name = "Your Name";
+console.log(name)
+
+// fight();
