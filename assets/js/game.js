3f3eab8aa71bdd18987abf31a45bf2ab75e6220b
Jeff Small <jeffmeister1990@gmail.com>
2022-05-11 01:13:55 -0400

    Completed initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 //You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -54,12 +55,21 @@ var fight = function() {
         }
     }
         else if (promptFight === "skip" || promptFight === "SKIP") {
-        window.alert(playerName + " has chosen to skip the fight!");
-        }   
-        
-        else {
-            window.alert("You need to choose a valid option. Try again!");
+            //Confirm player wants to skip
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+            //If yes (true), leave fight
+            if (confirmSkip) {
+                window.alert(playerName + " has decided tos kip this fight. Goodbye!");
+                
+                //Subtract money from playerMoney for skipping
+                playerMoney = playerMoney -2;
+            }
+
+            //If no (false), ask question again by running fight() again.
+            else {
+                fight();
+            }
         }
 };  
-
 fight();
